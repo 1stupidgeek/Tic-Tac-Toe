99db6d50cfb5c7d3dc5fd275a1197ba4ae4d8cf5
Arslan <arslanmudasir111@gmail.com>
2023-10-12 20:04:20 +0530

    connected deployed server and client

--- client/src/gameClient.js
+++ client/src/gameClient.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:3001");
+const socket = io("https://game-sever-9xxcdj4rv-madkid49.vercel.app/");
 
 function TicTacToeGame() {
   const [turn, setTurn] = useState(true);
